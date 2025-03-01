483b362a933bc6a895186f49282014e569c80a5b
Delta <julio_15@hotmail.co.uk>
2025-03-01 11:16:15 +0000

    Add landing page structure and styles, including navbar integration

--- src/php/partials/navbar.php
+++ src/php/partials/navbar.php
@@ -1,26 +1,31 @@
-<DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Stylesheets -->
-     <link rel="stylesheet" href="./css/navbar/navbar.css">
+    <link rel="stylesheet" type="text/css" href="../css/page.css">
+    <link rel="stylesheet" type="text/css" href="../css/navbar/navbar.css">
 </head>
 
 
 <body>
-
     <div class="topnav">
-        <a href="contact.html">Contact</a>
-        <a href="future.html">Future</a>
-        <a href="history.html">History</a>
-        <a href="home.html">Home</a>
+        <a href="index.php" name="home" style="float: left">Home</a>
+        <a href="pairs.php" name="memory">Play Pairs</a>
+        
+        <?php if ($condition) : ?>
+            echo '<a href="leaderboard.php">Leaderboard</a>
+        <?php else : ?>
+            <a href="registration.php">Register</a>
+        <?php endif; ?>
+
         <a href="javascript:void(0);" class="icon" onclick="navbar()"></a>
     </div>
 
-    <script src="./js/navbar.js"></script>
+    <script src="../js/navbar.js"></script>
 </body>
 </html>
