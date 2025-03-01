513006a095187e9e0e438e9c7cd0700fdcf9a367
Delta <julio_15@hotmail.co.uk>
2025-03-01 16:57:54 +0000

    Refactor index page styles by removing old CSS, adding new styles, and updating stylesheet links for improved organization and Bootstrap integration

--- src/php/index.php
+++ src/php/index.php
@@ -14,21 +14,24 @@ $registered = isset($_COOKIE['registered']) && $_COOKIE['registered'] === true;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Landing Page</title>
     <link rel="stylesheet" type="text/css" href="../css/page.css">
-    <link rel="stylesheet" type="text/css" href="../css/index_page/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/index_page/index_page.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <?php include 'partials/navbar.php'; ?>
     <div class="main">
-        <h1> Welcome To Pairs </h1>
-        <?php if (true) : ?>
-            <a type="button" href="pairs.php" class="btn btn-primary btn-lg">Click here to play</a>
-        <?php else : ?>
-            <p> You're not using a registered session? 
-                <a href="registration.php">Register now</a>
-            </p>
-        <?php endif; ?>
+        <div class="index_page">
+            <h1> Welcome To Pairs </h1>
+            <?php if (true) : ?>
+                <a type="button" href="pairs.php" class="btn btn-primary btn-lg">Click here to play</a>
+            <?php else : ?>
+                <p> You're not using a registered session? 
+                    <a href="registration.php">Register now</a>
+                </p>
+            <?php endif; ?>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
