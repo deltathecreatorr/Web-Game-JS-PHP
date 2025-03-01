09b2659c42fd4742d3ec4f0125762e90fe03bf2e
Delta <julio_15@hotmail.co.uk>
2025-03-01 12:03:41 +0000

    Enhance landing page with session management and dynamic content display

--- src/php/index.php
+++ src/php/index.php
@@ -1,3 +1,10 @@
+<?php
+
+session_start();
+$registered = isset($_SESSION['registered']) && $_SESSION['registered'] === true;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,11 +14,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Landing Page</title>
     <link rel="stylesheet" type="text/css" href="../css/page.css">
+    <link rel="stylesheet" type="text/css" href="../css/index_page/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
 </head>
 
 <body>
     <?php include 'partials/navbar.php'; ?>
     <div class="main">
+        <h1> Welcome to Pairs </h1>
+        <?php if ($registered) : ?>
+            <button type="button" href="pairs.php" class="btn btn-light btn-lg">Click here to play</button>
+        <?php else : ?>
+            <p> You're not using a registered session? 
+            <a href="registration.php">Register now</a>
+            </p>
+        <?php endif; ?>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
