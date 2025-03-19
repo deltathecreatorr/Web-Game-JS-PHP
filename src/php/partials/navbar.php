ec90942a4d3e65d3db87f76e91e083e6da89a105
Delta <julio_15@hotmail.co.uk>
2025-03-19 23:16:27 +0000

    Remove unnecessary comment for stylesheets in navbar.php

--- src/php/partials/navbar.php
+++ src/php/partials/navbar.php
@@ -25,7 +25,6 @@ if (isset($_COOKIE['avatar_config'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="../css/page.css">
     <link rel="stylesheet" type="text/css" href="../css/navbar/navbar.css">
     <link rel="stylesheet" type="text/css" href="../css/centered.css">
